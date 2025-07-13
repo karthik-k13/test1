32869e610f55dfe99fc8fef28d2342f8b46032cd
karthik-k13 <karthikreddy55015@gmail.com>
2025-07-13 21:18:38 +0530

    initial commit

---
+++ test1/src/main/java/com/demo/test1/Test1Application.java
@@ -0,0 +1,14 @@
+package com.demo.test1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Test1Application {
+
+	public static void main(String[] args) {
+		int x=10;
+		SpringApplication.run(Test1Application.class, args);
+	}
+
+}
